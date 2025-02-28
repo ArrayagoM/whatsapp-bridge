af2511e548120ebccbc5355e35790315a927f7e0
ArrayagoM <juanmartinarrayago@gmail.com>
2025-02-28 17:57:28 -0500

    vercel.json

--- src/models/Account.js
+++ src/models/Account.js
@@ -4,8 +4,8 @@ const accountSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     phoneNumber: { type: String, required: true, unique: true },
-    isAuthenticated: { type: Boolean, default: false }, // Estado de autenticación
-    clientId: { type: String, default: null }, // Identificador de la sesión
+    isAuthenticated: { type: Boolean, default: false },
+    clientId: { type: String, default: null },
   },
   { timestamps: true }
 );
